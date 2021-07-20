407e0108dd28607faa695d0a8a393a370ac58cb0
shouldnotbehere <87031997+shouldnotbehere@users.noreply.github.com>
2021-07-19 20:57:30 -0400

    tarea

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var simplexMenu = document.getElementById('simplex-menu');
+var show = document.getElementById('menu');
+
+simplexMenu.addEventListener('click', function(){
+    this.classList.toggle("close");
+    show.classList.toggle("show");
+})
+menu.addEventListener('click', function(){
+    location.reload()
+
+})
